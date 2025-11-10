82e303a34569c39936e5571c5655dff4cc4fc3fb
maria <marjuriba@alu.edu.gva.es>
2025-11-10 10:42:02 +0100

    Puta mierda a tomar por culo

--- Proyecto-Roguelite/src/Entidades/Sistema/GestorLogros.java
+++
@@ -1,8 +0,0 @@
-package Entidades.Sistema;
-
-public class GestorLogros {
-    private static final String CARPETA_DATOS = "datos";
-    private static final String CARPETA_PARTIDAS = "partidas";
-    private static final String ARCHIVO_DEFINICIONES = "logros_definiciones.json";
-    private static final String ARCHIVO_PROGRESO = "logros_progreso.json";
-}
