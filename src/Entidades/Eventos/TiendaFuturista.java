c14a47c5a5b860152a1ad95c5a8233da107ec2cc
maria <marjuriba@alu.edu.gva.es>
2025-11-10 10:40:48 +0100

    Traida src de master

---
+++ src/Entidades/Eventos/TiendaFuturista.java
@@ -0,0 +1,46 @@
+package Entidades.Eventos;
+
+import Entidades.Entidad;
+import Entidades.Jugador;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TiendaFuturista extends Entidad {
+
+    public TiendaFuturista() {
+        super("EVENTO_TIENDA_FUTURISTA", "Tienda Futurista",
+                "Un dron flotante con voz de vendedor te sigue emitiendo luces LED.\n" +
+                        "“¡Inversor visionario detectado! Compra NFTs de aire, cursos exclusivos de productividad y una IA que hará tu trabajo por ti (y el mío también).”",
+                "¿El dron te apunta con una pantalla holográfica:\n" +
+                        "“¿Quieres ser tu propio jefe en el metaverso?”");
+    }
+
+    @Override
+    public List<String> getInteraccionesBase() {
+        return Arrays.asList(
+                "1. Comprar NFTs",
+                "2. Pedirle un curso gratuito de criptomonedas",
+                "3. No comprar nada",
+                "4. Preguntar por productos específicos"
+        );
+    }
+
+    @Override
+    public String interactuar(Jugador jugador, String seleccion) {
+        switch (seleccion) {
+            case "1. Comprar NFTs":
+                jugador.reducirInteligencia(3);
+                return "El dron se ríe y se aleja a toda velocidad. El NFT desaparece en tus manos. **Pierdes 3 de Inteligencia**";
+            case "2. Pedirle un curso gratuito de criptomonedas":
+                jugador.aumentarCarisma(2);
+                return "No aprendes a gestionar tus finanzas, pero mejora tu poder de convicción. **Ganas 2 de Carisma**";
+            case "3. No comprar nada":
+                return "Te alejas sin mirar atrás, evitando caer en la trampa del consumismo digital.";
+            case "4. Preguntar por productos específicos":
+                return "El dron te ofrece una suscripción premium a un asistente virtual por 25 créditos. Decides que no vale la pena.";
+            default:
+                return "No entiendes bien qué hacer.";
+        }
+    }
+}
---
+++ src/Entidades/Eventos/WifiFantasma.java
@@ -0,0 +1,52 @@
+package Entidades.Eventos;
+
+import Entidades.Entidad;
+import Entidades.Jugador;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class WifiFantasma extends Entidad {
+
+    public WifiFantasma() {
+        super("EVENTO_WIFI_FANTASMA", "Wifi Fantasma",
+                "Detectas una señal WiFi con nombre “LAN_del_Más_Acá”.",
+                "Te conecta a la red ¿qué buscas?");
+    }
+
+    @Override
+    public List<String> getInteraccionesBase() {
+        return Arrays.asList(
+                "1. Entrar en bolsa",
+                "2. Entrar a un casino online",
+                "3. Buscar videos de gatos",
+                "4. Entrar a YouTube profundo"
+        );
+    }
+
+    @Override
+    public String interactuar(Jugador jugador, String seleccion) {
+        switch (seleccion) {
+            case "1. Entrar en bolsa":
+                return "Pones todas tus ganancias en una compañía llamada 'NoJewsTafa'.\n" +
+                        "La compañía cae en picado y te sientes un poco triste por dejar a tu familia sin comer otro mes.\n" +
+                        "Sigues adelante.";
+            case "2. Entrar a un casino online":
+                if (jugador.getSuerte() == 20){
+                    jugador.reducirSuerte(5);
+                    return "Lo metes todo al verde. Recapacitas y, rápidamente, lo cambias todo al rojo. Sale verde. **Pierdes 5 de suerte.**";
+                }else{
+                    jugador.aumentarSuerte(5);
+                    return "Lo metes todo al verde. No lo piensas dos veces. Sale verde. **Ganas 5 de suerte.**";
+                }
+            case "3. Buscar videos de gatos":
+                jugador.aumentarCordura(1);
+                return "Encuentras el video perfecto. **Ganas 1 de Cordura.**";
+            case "4. Entrar a YouTube profundo":
+                return "Encuentras un video que explica detalladamente como refinar Uranio-235.\n" +
+                        "Observas ensimismado, memorizas los pasos y das like. **Obtienes Uranio-235**"; /*AÑADIR OBJETO*/
+            default:
+                return "No entiendes bien qué hacer.";
+        }
+    }
+}
