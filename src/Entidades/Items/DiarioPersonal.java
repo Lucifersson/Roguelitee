7d6462a2e72ffc424feb07d77bbde78aed41fcf5
Lucifersson <mcastillol1906@gmail.com>
2025-11-12 08:48:43 +0100

    Re subida de item porque ha petao

---
+++ src/Entidades/Items/DiarioPersonal.java
@@ -0,0 +1,24 @@
+package Entidades;
+
+public class DiarioPersonal extends Item {
+
+	/**
+	 * Objeto pasivo: Diario Personal.
+	 * Bonificaciones: +2 Inteligencia, +1 Cordura.
+	 * Penalización: -1 Intimidación.
+	 */
+	public DiarioPersonal() {
+		super(
+				"ITEM_DIARIO_001", // idMySQL
+				"Diario Personal", // nombre
+				"Un libro de cuero gastado con notas privadas. Fomenta la calma y el análisis.",
+				75, // valor (Bajo, objeto inicial o común)
+
+				1, // modCordura: +1 (Aumenta la calma y el bienestar mental)
+				0, // modCarisma
+				-1, // modIntimidacion: -1 (Te hace parecer menos amenazante)
+				2, // modInteligencia: +2 (Fomenta la reflexión y el análisis)
+				0 // modSuerte
+		);
+	}
+}
